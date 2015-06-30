64bc639e3e48a49d2daff48bcb83c73d1d78a7bb
Olavi Mustanoja <tilastokeskus@gmail.com>
2015-06-30 19:48:33 +0300

    webhook config

--- www/public_html/git.php
+++ www/public_html/git.php
@@ -1,4 +1,6 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../include/psl-config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../include/Handle.php';
 use GitHubWebhook\Handler;
 
 $handler = new Handler(SECRET, __DIR__);
