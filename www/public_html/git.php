8a22d4810834f4e0049dbe73ece763dcb6dcd299
Olavi Mustanoja <tilastokeskus@gmail.com>
2015-06-30 19:20:47 +0300

    webhook config

--- www/public_html/git.php
+++ www/public_html/git.php
@@ -1,1 +1,9 @@
-<?php `git pull`;
+<?php
+use GitHubWebhook\Handler;
+
+$handler = new Handler(SECRET, __DIR__);
+if ($handler->handle()) {
+    echo "Repository pulled.";
+} else {
+    echo "Invalid request.";
+}
