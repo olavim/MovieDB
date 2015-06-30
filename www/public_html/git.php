41babcaa95f86b53d22e6e9b7c85ca1e68ae15fa
Olavi Mustanoja <tilastokeskus@gmail.com>
2015-06-30 19:49:14 +0300

    webhook test

--- www/public_html/git.php
+++ www/public_html/git.php
@@ -2,7 +2,7 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../include/psl-config.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../include/Handle.php';
 use GitHubWebhook\Handler;
-
+// webhook test
 $handler = new Handler(SECRET, __DIR__);
 if ($handler->handle()) {
     echo "Repository pulled.";
