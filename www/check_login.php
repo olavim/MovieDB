76ccce490a51d42130ea11b71c9fb208e4e67938
Olavi Mustanoja <tilastokeskus@gmail.com>
2015-07-01 15:42:16 +0300

    Working on login form

--- www/check_login.php
+++ www/check_login.php
@@ -4,9 +4,8 @@ include_once 'include/functions.php';
 
 sec_session_start();
 
-$rel_path = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT'])) . '/';
-
 if (login_check($mysqli) != true) {
+	$path = dirname($_SERVER['SCRIPT_NAME']);
 ?>
 <!doctype html>
 <html>
@@ -14,14 +13,14 @@ if (login_check($mysqli) != true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://fonts.googleapis.com/css?family=Roboto:100" rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?=$rel_path?>styles/jquery.mobile-1.4.5.min.css">
-    <link rel="stylesheet" href="<?=$rel_path?>styles/global.css">
-    <link rel="stylesheet" href="<?=$rel_path?>styles/default.css">
-    <link rel="stylesheet" href="<?=$rel_path?>styles/login.css">
-    <script src="<?=$rel_path?>js/jquery-1.11.3.min.js"></script>
-    <script src="<?=$rel_path?>js/jquery.mobile-1.4.5.min.js"></script>
-    <script src="<?=$rel_path?>js/sha512.js"></script>
-    <script src="<?=$rel_path?>js/forms.js"></script>
+    <link rel="stylesheet" href="<?=$path?>/styles/jquery.mobile-1.4.5.min.css">
+    <link rel="stylesheet" href="<?=$path?>/styles/global.css">
+    <link rel="stylesheet" href="<?=$path?>/styles/default.css">
+    <link rel="stylesheet" href="<?=$path?>/styles/login.css">
+    <script src="<?=$path?>/js/jquery-1.11.3.min.js"></script>
+    <script src="<?=$path?>/js/jquery.mobile-1.4.5.min.js"></script>
+    <script src="<?=$path?>/js/sha512.js"></script>
+    <script src="<?=$path?>/js/forms.js"></script>
 </head>
 <body>
 <div data-role="page">
